52091fdbbe0bc3da46f44a176720232f7b8a00fd
Dmitry <gomerty15@mail.ru>
2021-05-17 15:22:10 +0300

    добавлено решение

---
+++ zadacha 4/zadacha 4/zadacha 4.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+int main()
+{
+    int m;
+    std::cin >> m;
+    int* a = new int[m];
+    int* b = new int[m];
+    int* c = new int[m];
+
+    
+
+    for (int i = 0; i < m; ++i) {
+        c[i] = rand() % 40;
+    }
+
+    int k = 0, l = 0;
+    for (int i = 0; i < m; ++i) {
+        if (c[i] % 2 == 0) {
+            a[k++] = c[i];
+        }
+        else {
+            b[l++] = c[i];
+        }
+    }
+
+    
+
+}
+
